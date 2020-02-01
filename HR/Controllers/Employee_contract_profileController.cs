cca7580e4f04b0c0af3985d75ecdcdb4c6af0f7a
Eslam-Yousef <eslam.yousef3382@gmail.com>
2020-02-01 23:46:38 +0200

    i

--- HR/Controllers/Employee_contract_profileController.cs
+++ HR/Controllers/Employee_contract_profileController.cs
@@ -101,7 +101,6 @@ namespace HR.Controllers
                     record.Adult_Tickets_No = model.Adult_Tickets_No;
                     record.Child_Tickets_No = model.Child_Tickets_No;
                     record.Tickets_Class_Tpyefam = model.Tickets_Class_Tpyefam;
-
                     record.Employee_ProfileId = model.Employee_ProfileId;
                     var Employee_ProfileId = int.Parse(model.Employee_ProfileId);
                     record.Employee_Profile = dbcontext.Employee_Profile.FirstOrDefault(m => m.ID == Employee_ProfileId);
@@ -111,7 +110,10 @@ namespace HR.Controllers
                     var pos = dbcontext.Employee_contract_profile.Add(record);
                     dbcontext.SaveChanges();
 
-
+                    if (command == "Submit")
+                    {
+                        return RedirectToAction("edit", "Employee_Profile", new { id = int.Parse(record.Employee_ProfileId) });
+                    }
                     return RedirectToAction("Index", new { id = model.Employee_ProfileId });
                 }
                 else
@@ -129,6 +131,149 @@ namespace HR.Controllers
                 return View(model);
             }
 
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                ViewBag.To_Airport = dbcontext.Air_ports.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.From_Airport = dbcontext.Air_ports.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.Contract_Type = dbcontext.Contract_Type.ToList().Select(m => new { Code = m.Contract_Type_Code + "------[" + m.Contract_Type_Description + ']', ID = m.ID });
+                ViewBag.Employee_Profile = dbcontext.Employee_Profile.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.Approved_date = dbcontext.Employee_Profile.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.id = id.ToString();
+                var record = dbcontext.Employee_contract_profile.FirstOrDefault(m => m.ID == id);
+
+              
+                if (record != null)
+                {
+                    return View(record);
+                }
+                else
+                {
+                    TempData["Message"] = "There is no Employee Contract Profile";
+                    return View();
+                }
+            }
+            catch
+            (Exception e)
+            { return View(); }
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee_contract_profile model, string command)
+        {
+            try
+            {
+                if (model.ContractTypeId == null) { model.ContractTypeId = "0"; }
+                if (model.ApprovedbyId == null) { model.ApprovedbyId = "0"; }
+                if (model.FromAirpotId == null) { model.FromAirpotId = "0"; }
+                if (model.ToAirpotId == null) { model.ToAirpotId = "0"; }
+
+                ViewBag.To_Airport = dbcontext.Air_ports.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.From_Airport = dbcontext.Air_ports.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.Contract_Type = dbcontext.Contract_Type.ToList().Select(m => new { Code = m.Contract_Type_Code + "------[" + m.Contract_Type_Description + ']', ID = m.ID });
+                ViewBag.Employee_Profile = dbcontext.Employee_Profile.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+                ViewBag.Approved_date = dbcontext.Employee_Profile.ToList().Select(m => new { Code = m.Code + "------[" + m.Name + ']', ID = m.ID });
+
+                ViewBag.id = model.ID;
+                var prof = int.Parse(model.Employee_ProfileId);
+                //   var emp = dbcontext.Employee_Profile.FirstOrDefault(m => m.ID == prof);
+                var record = dbcontext.Employee_contract_profile.FirstOrDefault(m => m.ID == model.ID);
+                var emp = record.Employee_Profile;
+                record.Code = model.Code;
+                record.Contract = model.Contract;
+                record.Employment_type = model.Employment_type;
+                record.Contract_start_date = model.Contract_start_date;
+                record.Contract_end_date = model.Contract_end_date;
+                if (model.Contract_start_date > model.Contract_end_date)
+                {
+                    TempData["Message"] = "From date Issue bigger date Expire";
+                    return View(model);
+                }
+                record.Years = model.Years;
+                record.Months = model.Months;
+                record.Approved_date = model.Approved_date;
+                record.Notes = model.Notes;
+                record.Medical_date = model.Medical_date;
+                record.Medical_entity_name = model.Medical_entity_name;
+                record.Not_fit_reason = model.Not_fit_reason;
+                record.Medical_commite_comments = model.Medical_commite_comments;
+                record.Medical_commite_recomindation = model.Medical_commite_recomindation;
+                record.Result = model.Result;
+                record.Days_Balance = model.Days_Balance;
+                record.Days_Per_Period = model.Days_Per_Period;
+                record.Tickets_No = model.Tickets_No;
+                record.Tickets_Per_Period = model.Tickets_Per_Period;
+                record.Tickets_Class_Tpyeemp = model.Tickets_Class_Tpyeemp;
+                record.FromAirpotId = model.FromAirpotId;
+                record.ToAirpotId = model.ToAirpotId;
+                record.Air_ports = model.Air_ports;
+                record.Adult_Tickets_No = model.Adult_Tickets_No;
+                record.Child_Tickets_No = model.Child_Tickets_No;
+                record.Tickets_Class_Tpyefam = model.Tickets_Class_Tpyefam;
+
+                record.Employee_ProfileId = model.Employee_ProfileId;
+                var Employee_ProfileId = int.Parse(model.Employee_ProfileId);
+                record.Employee_Profile = dbcontext.Employee_Profile.FirstOrDefault(m => m.ID == Employee_ProfileId);
+                record.ContractTypeId = model.ContractTypeId;
+                record.ApprovedbyId = model.ApprovedbyId;
+                dbcontext.SaveChanges();
+
+                if (command == "Submit")
+                {
+                    return RedirectToAction("edit", "Employee_Profile", new { id = int.Parse(record.Employee_ProfileId) });
+                }
+                return RedirectToAction("index", new { id = model.Employee_ProfileId });
+            }
+            catch (DbUpdateException e)
+            {
+                TempData["Message"] = "This code Is already exists";
+                return View(model);
+            }
+            catch (Exception e)
+            { return View(model); }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var record = dbcontext.Employee_contract_profile.FirstOrDefault(m => m.ID == id);
+                if (record != null)
+                { return View(record); }
+                else
+                {
+                    TempData["Message"] = "There is no Employee Contract Profile";
+                    return View();
+                }
+
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
+
+        }
+        [ActionName("Delete")]
+        [HttpPost]
+        public ActionResult Deletemethod(int id)
+        {
+            var record = dbcontext.Employee_contract_profile.FirstOrDefault(m => m.ID == id);
+
+            try
+            {
+                dbcontext.Employee_contract_profile.Remove(record);
+                dbcontext.SaveChanges();
+                return RedirectToAction("index");
+            }
+            catch (DbUpdateException)
+            {
+                TempData["Message"] = "You can't delete beacause it have child";
+                return View(record);
+            }
+            catch (Exception e)
+            {
+                return View();
+            }
         }
     }
 }
